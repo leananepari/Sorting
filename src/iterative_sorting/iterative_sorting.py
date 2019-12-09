f6488a84a342f52e1925ce106764a73bf37a4276
Leana <leananeparidze.com>
2019-12-09 13:58:36 -0800

    completes selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -6,15 +6,17 @@ def selection_sort( arr ):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for j in range(cur_index + 1, len(arr)):
+          if arr[j] < arr[smallest_index]:
+            smallest_index = j   
 
 
 
         # TO-DO: swap
-
-
-
-
+        saveSmallest = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index] 
+        arr[cur_index] = saveSmallest
+    
     return arr
 
 
